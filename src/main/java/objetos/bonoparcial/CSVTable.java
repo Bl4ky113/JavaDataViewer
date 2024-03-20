a88daf4ab2dbe24c224c2945bc3594f98212d898
juan2005elpapu <judapelo2005@gmail.com>
2024-03-20 18:42:59 -0500

    imports no utilizados quitados

--- src/main/java/objetos/bonoparcial/CSVTable.java
+++ src/main/java/objetos/bonoparcial/CSVTable.java
@@ -2,7 +2,7 @@
 package objetos.bonoparcial;
 
 import javax.swing.JTable;
-import javax.swing.table.TableModel;
+
 
 // Intento de implementar una SubClase de JTable para los CSVs.
 // no se pudo :c
--- src/main/java/objetos/bonoparcial/WindowMenu.java
+++ src/main/java/objetos/bonoparcial/WindowMenu.java
@@ -2,8 +2,7 @@
 package objetos.bonoparcial;
 
 import java.awt.event.KeyEvent;
-import java.io.File;
-import java.io.IOException;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.ItemListener;
@@ -14,9 +13,9 @@ import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import javax.swing.UIManager;
 import javax.swing.Icon;
-import javax.swing.JFileChooser;
 
-import java.util.ArrayList;
+
+
 
 public class WindowMenu extends JMenuBar implements ActionListener, ItemListener {
   FileReader csvReader;
